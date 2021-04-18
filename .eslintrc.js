f7b476102b02ab54aa741cad81ccc91d6e353da1
over-engineer <dev@over-engineer.com>
2021-04-18 18:58:25 +0300

    Add eslint as a development dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-    "extends": "airbnb-base"
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
 };
